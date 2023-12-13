6a5514579592e119fc8c88b78a7e4fe71bfec1c9
Alfahaaan <ali.farhan160@gmail.com>
2023-12-13 21:22:22 +0700

    3-type-data-string

---
+++ 3-type-data/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Mohamad")
+	fmt.Println("Mohamad Ali")
+	fmt.Println("Mohamad Ali Farhan")
+
+	fmt.Println(len("Mohamad"))
+	fmt.Println("Mohamad Ali"[0])
+	fmt.Println("Mohamad Ali Farhan"[1])
+}
