352f8ff09521bf6365e0c0fe3e730993015d49c7
Alfahaaan <ali.farhan160@gmail.com>
2023-12-14 14:43:47 +0700

    3-type-data-matematika

---
+++ 3-type-data/matematika.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 10
+	var c = a + b
+	var d = a - b
+	var e = a * b
+	var f = a / b
+
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+
+	var i = 10
+	i += 10 // i = i + 10
+	fmt.Println(i)
+
+	i += 5 // i = i + 5
+	fmt.Println(i)
+}
