72224abc935577a6f173a370494db0edb942d877
Alfahaaan <ali.farhan160@gmail.com>
2023-12-17 11:16:02 +0700

    9-comment

---
+++ 9-function/comment.go
@@ -0,0 +1,11 @@
+package main
+
+/*
+this comment multiline comment
+this comment multiline comment
+this comment multiline comment
+this comment multiline comment
+*/
+func main() {
+	// this comment single line
+}
