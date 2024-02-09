28e4c8ac3cf6a10665bc43a33b54a8a6ad85f9ab
Alfahaaan <ali.farhan160@gmail.com>
2024-02-09 09:46:34 +0700

    15-access-modifier

--- 15-import/import.go
+++ 15-import/import.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	result := helper.SayHello("Alfa")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application)
+	fmt.Println(helper.vesion)            // tidak bisa diakses
+	fmt.Println(helper.sayGoodBye("Eko")) // tidak bisa diakses
 }
